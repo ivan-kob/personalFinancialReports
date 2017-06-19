f18bccaf223a8d71e8b9ef3ee8561aa7e3fef6eb
Edwin Perez <edwin@gammapartners.com>
2017-06-18 20:49:25 -0600

    create solution's repository, add basic layers to the prohect

---
+++ PersonalFinancialReports/ConsoleView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFinancialReports
+{
+    class ConsoleView
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
